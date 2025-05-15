f36886d8e57ccc356fc1e0051e65bde81b029d4d
akash100a <arakash968786@gmail.com>
2025-05-16 00:47:47 +0600

    feat: Add Company Data Viewer, Registration, and Success Components
    - Implemented CompanyDataViewer to fetch and display registered companies.
    - Created CompanyRegSuc to show a success message after company registration.
    - Developed CompanyRegistration form with validation and API integration for company registration.
    - Updated RootLayout to include a sidebar and navbar for better navigation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import RootLayout from "./components/main/RootLayout";
+// React Router dom........
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import CompanyDataViewer from "./components/company/CompanyDataViewer";
+import CompanyRegistration from "./components/company/CompanyRegistration";
+import CompanyRegSuc from "./components/company/CompanyRegSuc";
+import Dashboard from "./components/daahboard/Dashboard";
+import Demo from "./components/demo/Demo";
+import Demo2 from "./components/demo/Demo2";
+import Demo3 from "./components/demo/Demo3";
+import Demo4 from "./components/demo/Demo4";
+import Demo5 from "./components/demo/Demo5";
+import Demo6 from "./components/demo/Demo6";
+import InviteUser from "./components/InviteUser";
+import Reg1 from "./components/layers/Reg1";
+import Reg2 from "./components/layers/Reg2";
+import Login from "./components/login/Login";
+import Password from "./components/Password";
+import Register from "./components/registers/Register";
+import Register2 from "./components/registers/Register2";
+import RegistrationSuccess from "./components/registers/RegistrationSuccess";
+import Services from "./components/services/Services";
+import LoginProtected from "./Protect Rule/LoginProtected";
+import ProtectedCompany from "./Protect Rule/ProtectedCompany";
+import Residualanalysis from "./components/residualanalysis/Residualanalysis";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index path="/" element={<Register />}></Route>
+      <Route index path="/" element={<Register />}></Route>
+      <Route index path="/register2" element={<Register2 />}></Route>
+      <Route path="/inviteuser" element={<InviteUser />}></Route>
+      <Route path="/demo" element={<Demo />}></Route>
+      <Route path="/password" element={<Password />}></Route>
+      <Route path="/regsuccess" element={<RegistrationSuccess />}></Route>
+      <Route path="/demo2" element={<Demo2 />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/demo3" element={<Demo3 />}></Route>
+      <Route path="/demo5" element={<Demo5 />}></Route>
+      <Route path="/demo6" element={<Demo6 />}></Route>
+      <Route path="/residualanalysis" element={<Residualanalysis />}></Route>
+      <Route path="/reg1" component={Reg1} />
+      <Route path="/reg2" component={Reg2} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedCompany>
+            <Dashboard />
+          </ProtectedCompany>
+        }
+      ></Route>
+      <Route
+        path="/companyregistration"
+        element={
+          <LoginProtected>
+            <CompanyRegistration />
+          </LoginProtected>
+        }
+      ></Route>
+      <Route path="/companyregSuc" element={<CompanyRegSuc />}></Route>
+      <Route path="/companydata" element={<CompanyDataViewer />}></Route>
+      <Route path="/demo4" element={<Demo4 />}></Route>
+      <Route path="/services" element={<Services />}></Route>
+      <Route path="*" element={<Error />} />
+    </Route>
   )
-}
+);
 
-export default App
+const App = () => {
+  return (
+    <div className="h-[100vh]">
+      {/* //Router Provider..... */}
+      <RouterProvider router={router} />
+    </div>
+  );
+};
+
+export default App;
